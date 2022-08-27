4aa4f36721bb862d22a39de4073f1a425b0cee45
foysal <foysalf652@gmail.com>
2022-08-27 23:31:03 +0600

    email functions add

--- core/settings.py
+++ core/settings.py
@@ -145,8 +145,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'shopybusiness.co@gmail.com'
-EMAIL_HOST_PASSWORD  = 'gjmimnvoplafwaem'
+EMAIL_HOST_PASSWORD  = 'vbpmdomogfdhdrzl'
 DEFAULT_FROM_EMAIL  = 'inventory management <no-reply@inventory.localhost>'
+
 
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
